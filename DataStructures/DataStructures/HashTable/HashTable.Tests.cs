3af2ee4fe9cf5c912af0b8d7828058c8b106bebd
Korolev Dmitry <deagle.gross@gmail.com>
2021-06-14 15:27:14 +0300

    harsh dictionary implementation

--- DataStructures/DataStructures/HashTable/HashTable.Tests.cs
+++ DataStructures/DataStructures/HashTable/HashTable.Tests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
 // ReSharper disable InconsistentNaming
 
 namespace DataStructures.HashTable
@@ -11,7 +13,26 @@ namespace DataStructures.HashTable
 		{
 			var hashTable = new HashTable<int, int>();
 			
-			hashTable
-		}
+			hashTable.Add(1, 100);
+            hashTable.Add(2, 101);
+            hashTable.Add(3, 102);
+
+            hashTable.ContainsKey(1).ShouldBeTrue();
+            hashTable.ContainsKey(2).ShouldBeTrue();
+            hashTable.ContainsKey(3).ShouldBeTrue();
+            
+            hashTable.ContainsKey(4).ShouldBeFalse();
+        }
+
+        [TestMethod]
+        public void DeleteOperation()
+        {
+            var hashTable = new HashTable<int, int>();
+
+            hashTable.Add(1, 100);
+            hashTable.Remove(1);
+
+            hashTable.ContainsKey(1).ShouldBeFalse();
+        }
 	}
 }
--- DataStructures/DataStructures/HashTable/HashTable.cs
+++ DataStructures/DataStructures/HashTable/HashTable.cs
@@ -1,83 +1,260 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DataStructures.HashTable
 {
 	public class HashTable<TKey, TValue> : IDictionary<TKey, TValue>
 	{
-		private readonly int[] _buckets;
-		private readonly Entry<TKey, TValue>[] _entries;
+		/// <summary>
+		/// Defines structure containing key and value of dictionary
+		/// </summary>
+        private struct Entry<TKey, TValue>
+        {
+            public TKey Key;
+            public TValue Value;
 
-		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+            // if hashcode is null, then entry is not defined - this is a marker for entry
+            public int? HashCode;
+            public int? Next;
+        }
+
+        private const int InitialSize = 8;
+
+		private int[] _buckets;
+		private Entry<TKey, TValue>[] _entries;
+
+		/// <summary>
+		/// Index of first free entry
+		/// </summary>
+        private int _freeEntry;
+
+        private int _nextInsertedEntry;
+
+        private int _count;
+
+        public int Count => _count;
+
+        public HashTable(bool isReadOnly = false)
+        {
+            IsReadOnly = isReadOnly;
+            _count = 0;
+            
+            _freeEntry = -1;
+            _nextInsertedEntry = 0;
+
+            _buckets = new int[InitialSize];
+            _entries = new Entry<TKey, TValue>[InitialSize];
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
 		{
-			throw new System.NotImplementedException();
-		}
+            Add(item.Key, item.Value);
+        }
 
-		IEnumerator IEnumerable.GetEnumerator()
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return Remove(item.Key);
+        }
+
+        public void Add(TKey key, TValue value)
 		{
-			return GetEnumerator();
-		}
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key), "Key can not be null");
+            }
 
-		public void Add(KeyValuePair<TKey, TValue> item)
-		{
-			if (item.Key == null)
-			{
-				
-			}
-		}
+            var hashCode = key.GetHashCode() % _buckets.Length;
 
-		public void Clear()
-		{
-			throw new System.NotImplementedException();
-		}
+            // using free entry list to add new item
+            if (_freeEntry != -1)
+            {
+                _buckets[hashCode] = _freeEntry;
 
-		public bool Contains(KeyValuePair<TKey, TValue> item)
-		{
-			throw new System.NotImplementedException();
-		}
+                // setting free entry for next deleted item
+                _freeEntry = _entries[_freeEntry].Next ?? -1;
 
-		public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-		{
-			throw new System.NotImplementedException();
-		}
+                // saving element
+                _entries[_freeEntry].HashCode = hashCode;
+                _entries[_freeEntry].Next = null;
+                _entries[_freeEntry].Key = key;
+                _entries[_freeEntry].Value = value;
 
-		public bool Remove(KeyValuePair<TKey, TValue> item)
-		{
-			throw new System.NotImplementedException();
-		}
+                _count++;
+                return;
+            }
 
-		public int Count { get; }
+            // resize if there is no place to insert
+            if (_entries.Length == _count)
+            {
+                Array.Resize(ref _entries, _entries.Length * 2);
+            }
 
-		public bool IsReadOnly { get; }
+            // set next entries-chain
+            if (_buckets[hashCode] >= 0 && _buckets[hashCode] < _entries.Length)
+            {
+                var entry = _entries[_buckets[hashCode]];
+                while (entry.HashCode != null && entry.Next != null)
+                {
+                    entry = _entries[entry.Next.Value];
+                }
 
-		public void Add(TKey key, TValue value)
-		{
-			throw new System.NotImplementedException();
-		}
+                entry.Next = _nextInsertedEntry;
+            }
+
+            // insert new entry
+            _buckets[hashCode] = _nextInsertedEntry;
+            _entries[_nextInsertedEntry].HashCode = hashCode;
+            _entries[_nextInsertedEntry].Next = null;
+            _entries[_nextInsertedEntry].Key = key;
+            _entries[_nextInsertedEntry].Value = value;
+
+            _nextInsertedEntry++;
+            _count++;
+        }
 
 		public bool ContainsKey(TKey key)
 		{
-			throw new System.NotImplementedException();
-		}
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key), "Key can not be null");
+            }
+
+            var hashCode = key.GetHashCode() % _buckets.Length;
+
+            if (_buckets[hashCode] >= 0 && _buckets[hashCode] < _entries.Length)
+            {
+                var entry = _entries[_buckets[hashCode]];
+                while (entry.HashCode != null)
+                {
+                    if (entry.HashCode == hashCode)
+                    {
+                        return true;
+                    }
+
+                    if (entry.Next == null)
+                    {
+                        break;
+                    }
+
+                    entry = _entries[entry.Next.Value];
+                }
+            }
+
+            return false;
+        }
 
 		public bool Remove(TKey key)
 		{
-			throw new System.NotImplementedException();
-		}
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key), "Key can not be null");
+            }
 
-		public bool TryGetValue(TKey key, out TValue value)
-		{
-			throw new System.NotImplementedException();
-		}
+            var hashCode = key.GetHashCode() % _buckets.Length;
 
-		public TValue this[TKey key]
-		{
-			get => throw new System.NotImplementedException();
-			set => throw new System.NotImplementedException();
-		}
+            if (_buckets[hashCode] >= 0 && _buckets[hashCode] < _entries.Length)
+            {
+                Entry<TKey, TValue>? previousEntry = null;
+                var entry = _entries[_buckets[hashCode]];
 
-		public ICollection<TKey> Keys { get; }
+                while (entry.HashCode != null)
+                {
+                    if (entry.HashCode == hashCode)
+                    {
+                        // found exact entry to remove
+                        if (_freeEntry != -1)
+                        {
+                            var freeEntry = _entries[_freeEntry];
 
-		public ICollection<TValue> Values { get; }
-	}
+                            while (freeEntry.HashCode != null && freeEntry.Next != null)
+                            {
+                                freeEntry = _entries[freeEntry.Next.Value];
+                            }
+
+                            // fixing chain
+                            if (previousEntry.HasValue)
+                            {
+                                var previousEntry1 = previousEntry.Value;
+                                previousEntry1.Next = entry.Next;
+                                freeEntry.Next = previousEntry1.Next;
+                            }
+                        }
+                        else
+                        {
+                            _freeEntry = _buckets[entry.HashCode.Value];
+                        }
+
+                        // setting entry as null
+                        _entries[_buckets[entry.HashCode.Value]].Next = null;
+                        _entries[_buckets[entry.HashCode.Value]].HashCode = null;
+
+                        return true;
+                    }
+
+                    if (entry.Next == null)
+                    {
+                        break;
+                    }
+
+                    previousEntry = entry;
+                    entry = _entries[entry.Next.Value];
+                }
+            }
+
+            return false;
+        }
+
+        #region Not Implemented
+
+        public bool IsReadOnly { get; }
+
+        public ICollection<TKey> Keys => GetEntries().Select(entry => entry.Key).ToArray();
+
+        public ICollection<TValue> Values => GetEntries().Select(entry => entry.Value).ToArray();
+
+        private ICollection<Entry<TKey, TValue>> GetEntries()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TValue this[TKey key]
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
 }
