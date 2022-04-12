c540aee93aa4fee8c705b8210d65f585c9d3fd0a
Tony Skorik <just.skorik@gmail.com>
2022-04-12 16:26:55 +0400

    Add Spiral Order solution

---
+++ Leetcode/RandomTasks/PowXn.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCodeSolutions.RandomTasks
+{
+	[TestClass]
+	public class PowXn
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			double x = 2.00000;
+			int n = 10;
+
+			var result = MyPow(x, n);
+
+			result.ShouldBe(1024.00000);
+		}
+
+		[TestMethod]
+		public void Solve2()
+		{
+			double x = 2.10000;
+			int n = 3;
+
+			var result = MyPow(x, n);
+
+			result.ShouldBe(9.26100);
+		}
+
+		[TestMethod]
+		public void Solve3()
+		{
+			double x = 2.00000;
+			int n = -2;
+
+			var result = MyPow(x, n);
+
+			result.ShouldBe(0.25000);
+		}
+
+		[TestMethod]
+		public void Solve4()
+		{
+			double x = 1.00000;
+			int n = 2147483647;
+
+			var result = MyPow(x, n);
+
+			result.ShouldBe(1);
+		}
+
+		public double MyPow(double x, int n)
+		{
+			if (n == 0)
+			{
+				return 1;
+			}
+
+			if (x == 0)
+			{
+				return 0;
+			}
+
+			var num = x;
+			var pow = n;
+
+			if (n < 0)
+			{
+				num = 1 / num;
+				pow = -pow;
+			}
+
+			var ret = FastPow(num, pow);
+
+			return ret;
+		}
+
+		private double FastPow(double x, long n)
+		{
+			if (n == 0)
+			{
+				return 1.0;
+			}
+
+			var half = FastPow(x, n / 2);
+
+			if (n % 2 == 0)
+			{
+				return half * half;
+			}
+			else
+			{
+				return half * half * x;
+			}
+		}
+
+		public double MyPow_BruteForce(double x, int n)
+		{
+			if (n == 0)
+			{
+				return 1;
+			}
+
+			if (x == 0)
+			{
+				return 0;
+			}
+
+			var ret = x;
+			var mult = x;
+
+			if (n < 0)
+			{
+				ret = 1 / ret;
+				mult = 1 / mult;
+			}
+
+			for (int i = 1; i < (int)Math.Abs(n); i++)
+			{
+				ret *= mult;
+			}
+
+			return ret;
+		}
+	}
+}
---
+++ Leetcode/RandomTasks/SpiralMatrix.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client.Payloads;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCodeSolutions.RandomTasks
+{
+	[TestClass]
+	public class SpiralMatrix
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			int[][] matrix = new int[][]
+			{
+				new[] {1, 2, 3},
+				new[] {4, 5, 6},
+				new[] {7, 8, 9},
+			};
+
+			var result = SpiralOrder(matrix);
+			result.ShouldBe(new []{ 1, 2, 3, 6, 9, 8, 7, 4, 5 });
+		}
+
+		public IList<int> SpiralOrder(int[][] matrix)
+		{
+			var m = matrix.Length; // Y
+			var n = matrix[0].Length; // X
+
+			List<int> ret = new List<int>();
+			int totalNumbers = m * n;
+
+			int startX = 0;
+			int startY = 0;
+			int endX = n-1;
+			int endY = m-1;
+
+			while (ret.Count < totalNumbers)
+			{
+				ret.AddRange(GetFirstRow(matrix, startX, startY, endX, endY));
+				startY++;
+
+				ret.AddRange(GetLastCol(matrix, startX, startY, endX, endY));
+				endX--;
+
+				ret.AddRange(GetLastRowReversed(matrix, startX, startY, endX, endY));
+				endY--;
+
+				ret.AddRange(GetFirstColReversed(matrix, startX, startY, endX, endY));
+				startX++;
+			}
+
+			return ret;
+		}
+
+		public IList<int> GetFirstRow(int[][] matrix, int startX, int startY, int endX, int endY)
+		{
+			List<int> ret = new();
+			for (int col = startX; col <= endX; col++)
+			{
+				ret.Add(matrix[startY][col]);
+			}
+
+			return ret;
+		}
+
+		public IList<int> GetLastCol(int[][] matrix, int startX, int startY, int endX, int endY)
+		{
+			List<int> ret = new();
+			for (int row = startY; row <= endY; row++)
+			{
+				ret.Add(matrix[row][endX]);
+			}
+
+			return ret;
+		}
+
+		public IList<int> GetLastRowReversed(int[][] matrix, int startX, int startY, int endX, int endY)
+		{
+			List<int> ret = new();
+			for (int col = endX; col >= startX; col--)
+			{
+				ret.Add(matrix[endY][col]);
+			}
+
+			return ret;
+		}
+
+		public IList<int> GetFirstColReversed(int[][] matrix, int startX, int startY, int endX, int endY)
+		{
+			List<int> ret = new();
+			for (int row = endY; row >= startY; row--)
+			{
+				ret.Add(matrix[row][startX]);
+			}
+
+			return ret;
+		}
+	}
+}
