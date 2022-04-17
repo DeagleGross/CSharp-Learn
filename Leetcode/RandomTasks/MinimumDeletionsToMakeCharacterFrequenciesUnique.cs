16f02bdb09c39eb4c74f4334a55ae67b44ffa4af
Tony Skorik <just.skorik@gmail.com>
2022-04-17 19:42:40 +0400

    Add minimum deletions to make character frequncies qunique solution;

---
+++ Leetcode/RandomTasks/MinimumDeletionsToMakeCharacterFrequenciesUnique.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+// https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+
+namespace LeetCodeSolutions.RandomTasks
+{
+	[TestClass]
+	public class MinimumDeletionsToMakeCharacterFrequenciesUnique
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			string input = "aab";
+
+			var result = MinDeletions(input);
+
+			result.ShouldBe(0);
+		}
+
+		[TestMethod]
+		public void Solve2()
+		{
+			string input = "aaabbbcc";
+
+			var result = MinDeletions(input);
+
+			result.ShouldBe(2);
+		}
+
+		[TestMethod]
+		public void Solve3()
+		{
+			string input = "ceabaacb";
+
+			var result = MinDeletions(input);
+
+			result.ShouldBe(2);
+		}
+		
+		[TestMethod]
+		public void Solve4()
+		{
+			string input = "accdcdadddbaadbc";
+
+			var result = MinDeletions(input);
+
+			result.ShouldBe(1);
+		}
+		
+		[TestMethod]
+		public void Solve5()
+		{
+			string input = "abcabc";
+
+			var result = MinDeletions(input);
+
+			result.ShouldBe(3);
+		}
+
+		public int MinDeletions(string s)
+		{
+			var sorted = s.OrderByDescending(x => x).ToArray();
+
+			int deletions = 0;
+
+			char previousChar = sorted[0];
+
+			int currentFrequency = 1;
+
+			HashSet<int> encounteredFreqs = new HashSet<int>();
+
+			for (int i = 1; i < sorted.Length; i++)
+			{
+				if (sorted[i] == previousChar)
+				{
+					currentFrequency++;
+					continue;
+				}
+				else
+				{
+					// encountered new char
+
+					if (encounteredFreqs.Add(currentFrequency))
+					{
+						// new frequency - all ok
+						currentFrequency = 1;
+						previousChar = sorted[i];
+						continue;
+					}
+					else
+					{
+						while (currentFrequency > 0 && encounteredFreqs.Contains(currentFrequency))
+						{
+							currentFrequency--;
+							deletions++;
+						}
+
+						if (currentFrequency > 0)
+						{
+							encounteredFreqs.Add(currentFrequency);
+						}
+
+						currentFrequency = 1;
+						previousChar = sorted[i];
+						continue;
+					}
+				}
+			}
+
+			// process last char
+			while (currentFrequency > 0 && encounteredFreqs.Contains(currentFrequency))
+			{
+				currentFrequency--;
+				deletions++;
+			}
+
+			return deletions;
+		}
+	}
+}
