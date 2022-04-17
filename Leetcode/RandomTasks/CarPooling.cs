f3edba6c813b77eddb6b47c79a989a6f97eec5f6
Tony Skorik <just.skorik@gmail.com>
2022-04-17 23:15:25 +0400

    Add car pooling solution

---
+++ Leetcode/RandomTasks/CarPooling.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCodeSolutions.RandomTasks
+{
+	[TestClass]
+	public class CarPooling1
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			int[][] input = new int[][]
+			{
+				new []{ 2,1,5 },
+				new []{ 3,3,7 }
+			};
+
+			int capacity = 4;
+
+			var result = CarPooling(input, capacity);
+
+			result.ShouldBe(false);
+		}
+
+		[TestMethod]
+		public void Solve2()
+		{
+			int[][] input = new int[][]
+			{
+				new []{ 2,1,5 },
+				new []{ 3,3,7 }
+			};
+
+			int capacity = 5;
+
+			var result = CarPooling(input, capacity);
+
+			result.ShouldBe(true);
+		}
+
+		class TripPoint
+		{
+			public int KmPoint;
+			public int NumPassengers;
+		}
+
+		public bool CarPooling(int[][] trips, int capacity)
+		{
+			List<TripPoint> starts = new();
+			List<TripPoint> ends = new();
+
+			for (int trip = 0; trip < trips.Length; trip++)
+			{
+				starts.Add(new TripPoint()
+				{
+					KmPoint = trips[trip][1],
+					NumPassengers = trips[trip][0]
+				});
+
+				ends.Add(new TripPoint() {
+					KmPoint = trips[trip][2],
+					NumPassengers = trips[trip][0]
+				});
+			}
+
+			starts = starts.OrderBy(v => v.KmPoint).ToList();
+			ends = ends.OrderBy(v => v.KmPoint).ToList();
+
+			var start = 0;
+			var end = 0;
+
+			var currentCapacity = 0;
+
+			while (start < trips.Length)
+			{
+				var currentStart = starts[start];
+				var currentEnd = ends[end];
+
+				if (currentStart.KmPoint < currentEnd.KmPoint)
+				{
+					// ride started
+					currentCapacity += currentStart.NumPassengers;
+					if (currentCapacity > capacity)
+					{
+						return false;
+					}
+
+					start++;
+				}
+				else
+				{
+					// some ride has ended
+					currentCapacity -= currentEnd.NumPassengers;
+
+					end++;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Leetcode/RandomTasks/MeetingRooms2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace LeetCodeSolutions.RandomTasks
+{
+	[TestClass]
+	public class MeetingRooms2
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			int[][] input = new int[][]
+			{
+				new []{ 0, 30 },
+				new []{ 5, 10 },
+				new []{ 15, 20 }
+			};
+
+			var result = MinMeetingRooms(input);
+
+			result.ShouldBe(2);
+		}
+
+		public int MinMeetingRooms(int[][] intervals)
+		{
+			List<int> starts = new();
+			List<int> ends = new();
+			
+			for (int i = 0; i < intervals.Length; i++)
+			{
+				starts.Add(intervals[i][0]);
+				ends.Add(intervals[i][1]);
+			}
+
+			starts.Sort();
+			ends.Sort();
+
+			int start = 0;
+			int end = 0;
+
+			int ret = 0;
+
+			int currentConfs = 0;
+
+			while (start < starts.Count)
+			{
+				var currentStart = starts[start];
+				var currentEnd = ends[end];
+
+				if (currentStart < currentEnd)
+				{
+					currentConfs++;
+					ret = Math.Max(currentConfs, ret);
+					start++;
+				}
+				else
+				{
+					currentConfs--;
+					end++;
+				}
+			}
+
+			return ret;
+		}
+		
+	}
+}
