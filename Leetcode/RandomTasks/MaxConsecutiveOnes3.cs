72c6b1cfedbf31dd714e840467830df9c41f13a3
Tony Skorik <just.skorik@gmail.com>
2022-04-18 22:51:59 +0400

    Add word search solution;

---
+++ Leetcode/RandomTasks/MaxConsecutiveOnes3.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+// https://leetcode.com/problems/max-consecutive-ones-iii/
+//TODO: return to this one since looks like the solution is incorrect
+
+namespace LeetCodeSolutions.RandomTasks;
+
+[TestClass]
+public class MaxConsecutiveOnes3
+{
+	[TestMethod]
+	public void Solve()
+	{
+		int flipCount = 2;
+		var arr = GenerateArray(10);
+		PrintArray(arr);
+		var longestOnesResult = LongestOnes(arr, flipCount);
+		longestOnesResult.ShouldBe(6);
+	}
+
+	[TestMethod]
+	public void Solve2()
+	{
+		int flipCount = 0;
+		var arr = new []{0, 0, 1, 1, 1, 0, 0};
+		
+		var longestOnesResult = LongestOnes(arr, flipCount);
+		longestOnesResult.ShouldBe(3);
+	}
+
+	public int LongestOnes(int[] nums, int k)
+	{
+		int windowStart = 0;
+		var windowEnd = 0;
+		var maxWindowLength = 0;
+
+		Queue<int> zeroIndexes = new();
+
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (nums[i] == 0)
+			{
+				if (zeroIndexes.Count < k)
+				{
+					// we still have flips left
+					zeroIndexes.Enqueue(i);
+				}
+				else
+				{
+					// we don't have any flips left
+					var currentWindowLength = windowEnd - windowStart + 1;
+					if (currentWindowLength > maxWindowLength)
+					{
+						maxWindowLength = currentWindowLength;
+					}
+
+					windowStart = zeroIndexes.Dequeue() + 1;
+					zeroIndexes.Enqueue(i);
+				}
+			}
+
+			windowEnd = i;
+		}
+
+		var lastWindowLength = windowEnd - windowStart + 1;
+		if (lastWindowLength > maxWindowLength)
+		{
+			return lastWindowLength;
+		}
+
+		return maxWindowLength;
+	}
+
+	private int[] GenerateArray(int length)
+	{
+		Random rnd = new(1567);
+		return Enumerable.Range(0, length).Select(_ => rnd.Next(0, 2)).ToArray();
+	}
+
+	private void PrintArray(int[] target)
+	{
+		var ret = target.Aggregate(
+			new StringBuilder(),
+			(acc, value) =>
+			{
+				acc.Append(value);
+				return acc;
+			});
+
+		Console.WriteLine(ret);
+	}
+}
---
+++ Leetcode/RandomTasks/WordSearch.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+// https://leetcode.com/problems/word-search/
+
+namespace LeetCodeSolutions.RandomTasks
+{
+	[TestClass]
+	public class WordSearch
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			char[][] board = new char[][]
+			{
+				new []{ 'A', 'B', 'C', 'E' },
+				new []{ 'S', 'F', 'C', 'S' },
+				new []{ 'A', 'D', 'E', 'E' }
+			};
+
+			string word = "ABCCED";
+
+			var result = Exist(board, word);
+
+			result.ShouldBe(true);
+		}
+
+		[TestMethod]
+		public void Solve2()
+		{
+			char[][] board = new char[][]
+			{
+				new []{ 'A', 'B', 'C', 'E' },
+				new []{ 'S', 'F', 'C', 'S' },
+				new []{ 'A', 'D', 'E', 'E' }
+			};
+
+			string word = "ABCB";
+
+			var result = Exist(board, word);
+
+			result.ShouldBe(false);
+		}
+
+		[TestMethod]
+		public void Solve3()
+		{
+			char[][] board = new char[][]
+			{
+				new []{ 'a'},
+			};
+
+			string word = "a";
+
+			var result = Exist(board, word);
+
+			result.ShouldBe(true);
+		}
+
+		[TestMethod]
+		public void Solve4()
+		{
+			char[][] board = new char[][]
+			{
+				new []{ 'a', 'b'},
+				new []{ 'c', 'd'},
+			};
+
+			string word = "acdb";
+
+			var result = Exist(board, word);
+
+			result.ShouldBe(true);
+		}
+
+		[TestMethod]
+		public void Solve5()
+		{
+			char[][] board = new char[][]
+			{
+				new []{ 'A', 'B', 'C', 'E' },
+				new []{ 'S', 'F', 'E', 'S' },
+				new []{ 'A', 'D', 'E', 'E' }
+			};
+
+			string word = "ABCESEEEFS";
+
+			var result = Exist(board, word);
+
+			result.ShouldBe(true);
+		}
+
+		private int _rows;
+		private int _cols;
+
+		public bool Exist(char[][] board, string word)
+		{
+			_rows = board.Length;
+			_cols = board[0].Length;
+
+			bool[,] visited = new bool[_rows, _cols];
+			
+			for (int row = 0; row < _rows; row++)
+			{
+				for (var col = 0; col < _cols; col++)
+				{
+					if (board[row][col] == word[0])
+					{
+						if (word.Length == 1)
+						{
+							return true;
+						}
+
+						Array.Clear(visited);
+
+						var wordFound = SearchChar(board, row, col, word, 0, visited);
+						if (wordFound)
+						{
+							return true;
+						}
+					}
+				}
+			}
+
+			return false;
+		}
+
+		public bool SearchChar(char[][] board, int row, int col, string word, int wordPosition, bool[,] visited)
+		{
+			if (row < 0
+				|| row > _rows-1)
+			{
+				return false;
+			}
+
+			if (col < 0
+				|| col > _cols - 1)
+			{
+				return false;
+			}
+
+			if (wordPosition > word.Length - 1)
+			{
+				return true; // maybe false
+			}
+
+			if (visited[row, col] == true)
+			{
+				return false;
+			}
+
+			if (board[row][col] != word[wordPosition])
+			{
+				return false;
+			}
+
+
+			visited[row, col] = true; // set only correct word letters visited
+
+			var f1 = SearchChar(board, row, col + 1, word, wordPosition + 1, visited);
+			var f2 = SearchChar(board, row, col - 1, word, wordPosition + 1, visited);
+			var f3 = SearchChar(board, row + 1, col, word, wordPosition + 1, visited);
+			var f4 = SearchChar(board, row - 1, col, word, wordPosition + 1, visited);
+
+			visited[row, col] = false; // revert visited on this search path
+
+			return f1 || f2|| f3 || f4;
+		}
+	}
+}
