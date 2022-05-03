d5bb4ed832c969a946487243758618c5212ebec9
Tony Skorik <just.skorik@gmail.com>
2022-05-03 19:57:10 +0400

    Add some more solutions;

---
+++ Leetcode/RandomTasks/Strings/MaximumLengthOfAConcatenatedStringWithUniqueCharacters.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+
+namespace LeetCodeSolutions.RandomTasks.Strings
+{
+	[TestClass]
+	public class MaximumLengthOfAConcatenatedStringWithUniqueCharacters
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			string[] arr = new[] { "un", "iq", "ue" };
+
+			var result = MaxLength(arr);
+
+			result.Should().Be(4);
+		}
+
+		[TestMethod]
+		public void Solve2()
+		{
+			string[] arr = new[] { "cha", "r", "act", "ers" };
+
+			var result = MaxLength(arr);
+
+			result.Should().Be(6);
+		}
+
+		private int _maxLength = 0;
+		HashSet<char> _uniqueChars = new(26);
+
+		public int MaxLength(IList<string> arr)
+		{
+			HashSet<string> combination = new();
+
+			Backtrack(arr, 0, combination, 0);
+
+			return _maxLength;
+		}
+
+		// we can optimize this solution by storing only character counts in a Dictionary<char, int>
+		// instead of the HashSet<string>
+		private void Backtrack(IList<string> parts, int start, HashSet<string> combination, int combinationLength)
+		{
+			if (combinationLength > _maxLength
+				&& IsUnique(combination))
+			{
+				_maxLength = combinationLength;
+			}
+
+			if (start > parts.Count - 1
+				|| combination.Count == parts.Count)
+			{
+				// means all parts are used or nex part is beyond parts
+				return;
+			}
+
+			for (int i = start; i < parts.Count; i++)
+			{
+				var partAdded = combination.Add(parts[i]);
+
+				if (!partAdded)
+				{
+					// skip duplicates
+					continue;
+				}
+
+				var newCombinationLength = combinationLength + parts[i].Length;
+
+				Backtrack(parts, i + 1, combination, newCombinationLength);
+
+				combination.Remove(parts[i]);
+			}
+		}
+
+		private bool IsUnique(HashSet<string> combination)
+		{
+			_uniqueChars.Clear();
+
+			foreach (var part in combination)
+			{
+				foreach (var c in part)
+				{
+					if (!_uniqueChars.Add(c))
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Leetcode/RandomTasks/Strings/ValidateIPAddress.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// https://leetcode.com/problems/validate-ip-address/
+
+namespace LeetCodeSolutions.RandomTasks.Strings
+{
+	[TestClass]
+	public class ValidateIpAddress
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			string queryIp = "172.16.254.1";
+
+			var result = ValidIPAddress(queryIp);
+
+			result.Should().Be("IPv4");
+		}
+
+		private const string _ipV4 = "IPv4";
+		private const string _ipV6 = "IPv6";
+		private const string _neither = "Neither";
+
+		public string ValidIPAddress(string queryIP)
+		{
+			var ip = queryIP.ToLowerInvariant();
+			if (ip.Contains('.'))
+			{
+				return CheckV4(ip);
+			}
+
+			if (ip.Contains(':'))
+			{
+				return CheckV6(ip);
+			}
+
+			return _neither;
+		}
+
+		private string CheckV4(string ip)
+		{
+			// "x1.x2.x3.x4"
+			var parts = ip.Split('.');
+			if (parts.Length != 4)
+			{
+				return _neither;
+			}
+
+			foreach (var part in parts)
+			{
+				if ((part.Length == 0 || part.Length > 3)
+					|| (part.Length > 1
+						&& part.StartsWith('0')))
+				{
+					return _neither;
+				}
+
+				var parseSuccess = int.TryParse(
+					part,
+					NumberStyles.Integer,
+					CultureInfo.InvariantCulture,
+					out int result);
+
+				if (!parseSuccess || result > 255)
+				{
+					return _neither;
+				}
+			}
+
+			return _ipV4;
+		}
+
+		private string CheckV6(string ip)
+		{
+			//"x1:x2:x3:x4:x5:x6:x7:x8"
+			var parts = ip.Split(":");
+			if (parts.Length != 8)
+			{
+				return _neither;
+			}
+
+			foreach (var part in parts)
+			{
+				//65535
+				if (part.Length == 0
+					|| part.Length > 4)
+				{
+					return _neither;
+				}
+
+				var parseSuccess = int.TryParse(
+					part,
+					NumberStyles.HexNumber,
+					CultureInfo.InvariantCulture,
+					out int result);
+
+				if (!parseSuccess || result > 65535)
+				{
+					return _neither;
+				}
+			}
+
+			return _ipV6;
+		}
+	}
+}
