764f29d809fbe4179a1620bddd6197b76675411f
Tony Skorik <just.skorik@gmail.com>
2022-06-12 19:37:28 +0400

    Remove not working solution;

--- Leetcode/RandomTasks/DynamicProgramming/InterleavingString.cs
+++ Leetcode/RandomTasks/DynamicProgramming/InterleavingString.cs
@@ -39,7 +39,7 @@ namespace LeetCodeSolutions.RandomTasks.DynamicProgramming
 
 		public bool IsInterleave(string s1, string s2, string s3)
 		{
-			if (s3.Length > s1.Length + s2.Length)
+			if (s3.Length != s1.Length + s2.Length)
 			{
 				return false;
 			}
@@ -85,52 +85,6 @@ namespace LeetCodeSolutions.RandomTasks.DynamicProgramming
 				}
 			}
 			return dp[s1.Length,s2.Length];
-		}
-
-		public bool IsInterleave2(String s1, String s2, String s3)
-		{
-			if (s3.Length > s1.Length + s2.Length)
-			{
-				return false;
-			}
-
-			bool[] dp = new bool[s2.Length + 1];
-
-			for (int i = 0; i <= s1.Length; i++)
-			{
-				for (int j = 0; j <= s2.Length; j++)
-				{
-					if (i == 0 && j == 0)
-					{
-						dp[j] = true;
-						continue;
-					}
-
-					var targetCharToCompare = s3[i + j - 1];
-
-					if (i == 0)
-					{
-						dp[j] = dp[j - 1] && s2[j - 1] == targetCharToCompare;
-					}
-					else if (j == 0)
-					{
-						dp[j] = dp[j] && s1[i - 1] == targetCharToCompare;
-					}
-					else
-					{
-						dp[j] = (
-								dp[j]
-								&& s1[i - 1] == targetCharToCompare
-							)
-							|| (
-								dp[j - 1]
-								&& s2[j - 1] == targetCharToCompare
-							);
-					}
-				}
-			}
-
-			return dp[s2.Length];
 		}
 	}
 }
