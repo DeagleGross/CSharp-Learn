afa7265158cddd129d6cac647e1af7d20bc31660
Tony Skorik <just.skorik@gmail.com>
2022-06-12 21:07:53 +0400

    Add palindrome partitioning solution;

---
+++ Leetcode/RandomTasks/Backtracking/PalindromePartitioning.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// https://leetcode.com/problems/palindrome-partitioning/
+
+namespace LeetCodeSolutions.RandomTasks.Backtracking
+{
+	[TestClass]
+	public class PalindromePartitioning
+	{
+		[TestMethod]
+		public void Solve()
+		{
+			var s = "aab";
+
+			var result = Partition(s);
+
+			result.Count.Should().Be(2);
+		}
+
+		public IList<IList<string>> Partition(string s)
+		{
+			List<IList<string>> result = new();
+
+			var temp = new List<string>();
+
+			Backtrack(0, result, temp, s);
+
+			return result;
+		}
+
+		private void Backtrack(int start, List<IList<string>> result, List<string> currentPalindromeList, string s)
+		{
+			if (start >= s.Length)
+			{
+				// here we are certain that currentPalindromeList contains only palindromes
+				result.Add(new List<string>(currentPalindromeList));
+			}
+
+			for (int potentialPalindromeEnd = start; potentialPalindromeEnd < s.Length; potentialPalindromeEnd++)
+			{
+				if (IsPalindrome(s, start, potentialPalindromeEnd))
+				{
+					// add current substring in the currentList
+					currentPalindromeList.Add(s[start..(potentialPalindromeEnd + 1)]);
+
+					Backtrack(potentialPalindromeEnd + 1, result, currentPalindromeList, s);
+
+					// backtrack and remove the current substring from currentList
+
+					currentPalindromeList.RemoveAt(currentPalindromeList.Count - 1);
+				}
+			}
+		}
+
+		private bool IsPalindrome(string s, int low, int high)
+		{
+			while (low < high)
+			{
+				if (s[low] != s[high])
+				{
+					return false;
+				}
+
+				low++;
+				high--;
+			}
+			return true;
+		}
+	}
+}
--- Leetcode/RandomTasks/Strings/LongestPalindromicString.cs
+++ Leetcode/RandomTasks/Strings/LongestPalindromicString.cs
@@ -1,106 +1,151 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shouldly;
+
+// https://leetcode.com/problems/longest-palindromic-substring/
 
 namespace LeetCodeSolutions.RandomTasks.Strings;
 
 [TestClass]
 public class LongestPalindromicString
 {
-    [TestMethod]
-    public void Solve()
-    {
-        string input = "qweabaabaqwe";
-        var res = LongestPalindrome(input);
-        res.ShouldBe("abaaba");
-    }
+	[TestMethod]
+	public void Solve()
+	{
+		string input = "qweabaabaqwe";
+		var res = LongestPalindrome(input);
+		res.ShouldBe("abaaba");
+	}
 
-    static string LongestPalindrome(string s)
-    {
-        if (s.Length == 1)
-        {
-            return s;
-        }
+	public string LongestPalindrome(string s)
+	{
+		if (string.IsNullOrEmpty(s))
+		{
+			return "";
+		}
 
-        string result = s[0].ToString();
+		int start = 0; 
+		int end = 0;
 
-        int start = 0;
+		for (int i = 0; i < s.Length; i++)
+		{
+			// case with center on letter
+			int len1 = ExpandAroundCenter(s, i, i);
 
-        Dictionary<char, List<int>> letterPositions = new();
+			// case with center between letters
+			int len2 = ExpandAroundCenter(s, i, i + 1);
+
+			int len = Math.Max(len1, len2);
+
+			if (len > end - start)
+			{
+				start = i - (len - 1) / 2;
+				end = i + len / 2;
+			}
+		}
+		return s.Substring(start, end-start+1);
+	}
+
+	private int ExpandAroundCenter(string s, int left, int right)
+	{
+		int L = left;
+		int	R = right;
+		while (L >= 0 && R < s.Length && s[L] == s[R])
+		{
+			L--;
+			R++;
+		}
+
+		return R - L - 1;
+	}
+
+	static string LongestPalindrome_Old(string s)
+	{
+		if (s.Length == 1)
+		{
+			return s;
+		}
+
+		string result = s[0].ToString();
+
+		int start = 0;
+
+		Dictionary<char, List<int>> letterPositions = new();
 
 
-        void RemeberLetterPosition(char letter, int position)
-        {
-            if (!letterPositions.ContainsKey(letter))
-            {
-                letterPositions[letter] = new();
-            }
+		void RemeberLetterPosition(char letter, int position)
+		{
+			if (!letterPositions.ContainsKey(letter))
+			{
+				letterPositions[letter] = new();
+			}
 
-            letterPositions[letter].Add(position);
-        }
+			letterPositions[letter].Add(position);
+		}
 
-        for (int i = s.Length - 1; i >= 0; i--)
-        {
-            RemeberLetterPosition(s[i], i);
-        }
+		for (int i = s.Length - 1; i >= 0; i--)
+		{
+			RemeberLetterPosition(s[i], i);
+		}
 
-        while (start < s.Length - 1)
-        {
-            if (letterPositions[s[start]].Count <= 1)
-            {
-                start++;
-                continue;
-            }
+		while (start < s.Length - 1)
+		{
+			if (letterPositions[s[start]].Count <= 1)
+			{
+				start++;
+				continue;
+			}
 
-            int sameLetterIndex = 0;
+			int sameLetterIndex = 0;
 
-            while (sameLetterIndex < letterPositions[s[start]].Count)
-            {
-                var sameLetterPosition = letterPositions[s[start]][sameLetterIndex];
+			while (sameLetterIndex < letterPositions[s[start]].Count)
+			{
+				var sameLetterPosition = letterPositions[s[start]][sameLetterIndex];
 
-                if (sameLetterPosition < start)
-                {
-                    break;
-                }
+				if (sameLetterPosition < start)
+				{
+					break;
+				}
 
-                var left = start;
-                var right = sameLetterPosition;
-                var found = true;
+				var left = start;
+				var right = sameLetterPosition;
+				var found = true;
 
-                while (right - left > 1)
-                {
-                    left++;
-                    right--;
+				while (right - left > 1)
+				{
+					left++;
+					right--;
 
-                    if (s[left] != s[right])
-                    {
-                        found = false;
-                        break;
-                    }
-                }
+					if (s[left] != s[right])
+					{
+						found = false;
+						break;
+					}
+				}
 
-                if (found)
-                {
-                    var palindrome = s.Substring(start, sameLetterPosition - start + 1);
-                    if (palindrome.Length > result.Length)
-                    {
-                        result = palindrome;
-                    }
+				if (found)
+				{
+					var palindrome = s.Substring(start, sameLetterPosition - start + 1);
+					if (palindrome.Length > result.Length)
+					{
+						result = palindrome;
+					}
 
-                    break;
-                }
+					break;
+				}
 
-                sameLetterIndex++;
-            }
+				sameLetterIndex++;
+			}
 
-            start++;
+			start++;
 
-            if (result.Length >= s.Length - start)
-            {
-                break;
-            }
-        }
+			if (result.Length >= s.Length - start)
+			{
+				break;
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
